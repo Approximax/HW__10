96b320ced6c5f0cde16ae2227d672403ef45d3c6
Vladislav Kryukov <vlad_pw_v@list.ru>
2024-02-02 23:10:54 +0300

    added last parametrized test and names for each test

--- src/test/java/parametraized/SimpleParametrizedTests.java
+++ src/test/java/parametraized/SimpleParametrizedTests.java
@@ -7,13 +7,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 import java.util.stream.Stream;
 
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.CollectionCondition.texts;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleParametrizedTests{
@@ -33,7 +34,7 @@ public class SimpleParametrizedTests{
     }
 
     @EnumSource(Language.class)
-    @ParameterizedTest
+    @ParameterizedTest(name = "Заглавная страница должна отображаться в соответствии с выбранным языком")
     void yaMusicSiteShouldShowCorrectText(Language language) {
         $$(".d-lang-switcher").find(text(language.name())).click();
         $(".d-header__title").shouldHave(text(language.description));
@@ -53,9 +54,17 @@ public class SimpleParametrizedTests{
     }
 
     @MethodSource
-    @ParameterizedTest
+    @ParameterizedTest(name = "Кнопки на заглавной странице должны соответствовать выбранному языку")
     void yaMusicShouldShowCorrectButtons(Language language, List<String> expectedButtons) {
         $$(".d-lang-switcher").find(text(language.name())).click();
         $$(".d-tabs").filter(visible).shouldHave(texts(expectedButtons));
+    }
+
+    @ValueSource(strings = {"plenka", "Ghostemane", "Rammstein"})
+    @ParameterizedTest(name = "Для поискового запроса по исполнителю {0} должен отображаться не пустой список треков")
+    void searchResultsShouldNotBeEmpty(String searchQuery) {
+        $(".d-search__button").click();
+        $(".d-input__field").should(appear).setValue(searchQuery).pressEnter();
+        $$(".d-track").shouldBe(sizeGreaterThan(0));
     }
 }
